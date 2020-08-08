ac0a758604dba2394d48a3d904080874d142802a
Burak Kılıç <53341645+burak-kilic@users.noreply.github.com>
2020-08-08 14:29:08 +0300

    Add files via upload

---
+++ Passcode/Passcode/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Transactions;
+
+namespace Passcode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var code = "";
+            while (code != "secret")
+
+                {
+
+                Console.WriteLine("username :");
+                var username = Console.ReadLine();
+                Console.WriteLine("Password :");
+                code = Console.ReadLine();
+                if (code != "secret")
+                {
+                    Console.WriteLine("{0}'s password wrong.", username);
+                    
+                }
+
+                Console.WriteLine("{0} entered succesfully.", username);
+
+            }
+        }
+    }
+}
